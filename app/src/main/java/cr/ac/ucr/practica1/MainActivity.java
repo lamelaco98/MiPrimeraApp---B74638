96b282eda41b0c41f64a195cdcb37bbdeb037706
lamelaco98 <64126733+lamelaco98@users.noreply.github.com>
2020-08-21 18:43:37 -0600

    V2
    Mi Primera App actualizada a la clase del 21/08/2020

--- app/src/main/java/cr/ac/ucr/practica1/MainActivity.java
+++ app/src/main/java/cr/ac/ucr/practica1/MainActivity.java
@@ -4,14 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 import cr.ac.ucr.practica1.utils.AppPreferences;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ArrayList<String> todosArr;
+    private ArrayAdapter<String> todosAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,18 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // ListView: Muestra los elementos <----> ArrayAdapter: Intermediario <----> ArrayList: Almacena elementos
+
+        ListView lvTodos = findViewById(R.id.lv_todos);
+        todosArr = new ArrayList<>();
+
+        todosAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, todosArr);
+
+        lvTodos.setAdapter(todosAdapter);
+
+        //todosArr.add("Hola");
+        //todosArr.add("Mundo");
     }
 
     @Override
@@ -47,5 +72,43 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.fab_add_todo:
+                showAlert();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+        final View view = inflater.inflate(R.layout.dialog_add_task, null);
+
+        builder.setView(view)
+                .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        TextInputEditText etTaskName = view.findViewById(R.id.et_task_name);
+
+                        String taskName = etTaskName.getText().toString();
+
+                        if(!taskName.isEmpty()){
+                            todosArr.add(taskName);
+                            todosAdapter.notifyDataSetChanged(); // Adapter notifica que hubo cambios en el data set, es decir los datos que se les estan pasando
+                            dialog.dismiss();
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null);
+
+        builder.create();
+        builder.show();
     }
 }
